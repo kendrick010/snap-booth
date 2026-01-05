a72a89cbf5a9aa3269d619b1a7e250c3d670137a
kendrick010 <nguyen.kendrick@yahoo.com>
2026-01-05 15:47:40 -0800

    testing without gradient

--- software/snap-booth-website/components/StripeGradient.tsx
+++ software/snap-booth-website/components/StripeGradient.tsx
@@ -1,22 +1,22 @@
 "use client";
 
 import { useEffect, useRef } from "react";
-import { Gradient } from '@/lib/gradient.js'
+// import { Gradient } from '@/lib/gradient.js'
 
 export default function StripeGradient() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  useEffect(() => {
-    if (!canvasRef.current) return;
+  // useEffect(() => {
+  //   if (!canvasRef.current) return;
 
-    const gradient = new Gradient() as any;
-    gradient.initGradient("#gradient-canvas");
+  //   const gradient = new Gradient() as any;
+  //   gradient.initGradient("#gradient-canvas");
 
-    const handleResize = () => gradient.resize();
-    window.addEventListener("resize", handleResize);
+  //   const handleResize = () => gradient.resize();
+  //   window.addEventListener("resize", handleResize);
 
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
+  //   return () => window.removeEventListener("resize", handleResize);
+  // }, []);
 
   return (
     <canvas
